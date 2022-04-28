c6c4232f2eb41d76c10c11f5b9f0e93f3c15b88c
Sohan Vichare <sohanvichare@DN0a1f6544.SUNet>
2022-04-28 14:08:25 -0700

    domains: format generated term in readable way
    - this is essentially a translator from peano format to readable format
    - eg. given (= (+ 5 3) 8), it will return (5+3)=8

--- domains/generate.py
+++ domains/generate.py
@@ -54,3 +54,35 @@ def generate_term(degree: int, config: TermConfig) -> str:
         raise Exception(f"Unknown term format {term_format}")
 
     return gt_r(degree, config.complexity)
+
+
+def format(
+    term: str,
+) -> str:
+    """Formats an equation string to be human readable,
+    currently supports only binary operators that function
+    like +, e.g. + - / * = etc."""
+
+    def f_r(e: str) -> str:
+        # Base case, single term
+        if len(e) <= 1 or e[0] != "(" or e[-1] != ")":
+            return e
+        # Recursive case
+        e = e[1:-1]
+        operator = e.split(" ")[0]
+
+        def split(s: str) -> str:
+            stack = []
+            for i, c in enumerate(s):
+                if c == "(":
+                    stack.append(i)
+                elif c == ")" and stack:
+                    stack.pop()
+                if len(stack) == 0:
+                    return i + 1
+
+        e = e[2:]
+        s = split(e)
+        return f"({f_r(e[:s])}{operator}{f_r(e[s+1:])})"
+
+    return f_r(term)[1:-1]
