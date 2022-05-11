613804a92d933396d0a24188ca4c0388faedb3eb
Gabriel Poesia <poesia@stanford.edu>
2022-05-11 12:14:57 -0700

    Save run config to wandb

--- learning/main.py
+++ learning/main.py
@@ -14,16 +14,16 @@ from environment import SingleDomainEnvironment
 from domain import EquationsDomain
 
 
-def setup_wandb(jobCfg: DictConfig):
-    if jobCfg.get("wandb_project"):
-        wandb.init(project=jobCfg.wandb_project)
+def setup_wandb(cfg: DictConfig):
+    if cfg.job.get("wandb_project"):
+        wandb.init(project=cfg.job.wandb_project, config=cfg)
     else:
         # Disable wandb (i.e., make log() a no-op).
         wandb.log = lambda *args, **kwargs: None
 
 @hydra.main(config_path="config", config_name="test")
 def main(cfg: DictConfig):
-    setup_wandb(cfg.job)
+    setup_wandb(cfg)
 
     domain = EquationsDomain()
     # policy = DecisionTransformer(cfg['policy'])
