cdc0bb24e0be541375d65498c4338682aa617920
Gabriel Poesia <poesia@stanford.edu>
2022-06-16 14:16:34 -0700

    Add default configs for new commands

---
+++ learning/episode.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class ProofSearchEpisode:
+    success: bool
+    iterations: int
+    steps_added: int
+    steps_created: int
+    problem: str
+    solution: list[str]
+    visited_negatives: list[str]
+    discovered_negatives: list[str]
